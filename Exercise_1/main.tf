1ae9cdc28b8fa381ad1e2db453f2c486a9a9e45a
Anand P V <39004869+AdroitAnandAI@users.noreply.github.com>
2021-01-24 23:39:14 +0530

    Update main.tf

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -2,7 +2,7 @@
 
 provider "aws" {
   access_key = "AKIA4U47JUFEHLW57QJW"
-  secret_key = "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"
+  secret_key = **************
   region = "us-east-1"
   profile = "default"
 }
