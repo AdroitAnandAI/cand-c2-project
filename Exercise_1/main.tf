2a74d09db375e2e6336d8f53d9dcb7ebdd5f332c
Anand <ananduthaman@gmail.com>
2021-01-24 23:37:59 +0530

    adding all files

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,26 @@
 # TODO: Designate a cloud provider, region, and credentials
 
+provider "aws" {
+  access_key = "AKIA4U47JUFEHLW57QJW"
+  secret_key = "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"
+  region = "us-east-1"
+  profile = "default"
+}
+
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
 
+resource "aws_instance" "Udacity-T2" {
+  ami           = "ami-0be2609ba883822ec" # Amazon Linux 2 AMI (HVM), SSD in us east 1
+  instance_type = "t2.micro"
+  count         = 4
+}
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
+
+
+resource "aws_instance" "Udacity-M4" {
+  ami           = "ami-0be2609ba883822ec" # Amazon Linux 2 AMI (HVM), SSD in us east 1
+  instance_type = "m4.large"
+  count         = 2
+}
--- Exercise_2/variables.tf
+++ Exercise_2/variables.tf
@@ -1,1 +1,26 @@
 # TODO: Define the variable for aws_region
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "lambda_function_name" {
+  type    = string
+  default = "greet_lambda"
+}
+
+variable "runtime" {
+  type    = string
+  default = "python3.8"
+}
+
+variable "output_archive_name" {
+  type    = string
+  default = "greet_lambda.zip"
+}
+
+variable "lambda_handler" {
+  type    = string
+  default = "greet_lambda.lambda_handler"
+}
