39cc5e099845a3a94b197f1f20539f66eb15beb9
Anand <ananduthaman@gmail.com>
2021-01-25 12:32:32 +0530

    removed keys again

--- Exercise_2/main.tf
+++ Exercise_2/main.tf
@@ -2,8 +2,7 @@
 # https://www.terraform.io/docs/providers/aws/r/lambda_function.html
 
 provider "aws" {
-  access_key = "AKIA4U47JUFEHLW57QJW"
-  secret_key = "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"
+
   region  = var.region
   profile = "default"
 }
