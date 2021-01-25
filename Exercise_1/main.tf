d3bea183131a1dbb7b99b60255b0d93214b1a3f8
Anand <ananduthaman@gmail.com>
2021-01-25 12:28:31 +0530

    removed keys

---
+++ Exercise_1/main.tf
@@ -0,0 +1,26 @@
+# TODO: Designate a cloud provider, region, and credentials
+
+provider "aws" {
+  access_key = "AKIA4U47JUFEEKTGN6JW"
+  secret_key = "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"
+  region = "us-east-1"
+  profile = "default"
+}
+
+
+# TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
+
+resource "aws_instance" "Udacity-T2" {
+  ami           = "ami-0be2609ba883822ec" # Amazon Linux 2 AMI (HVM), SSD in us east 1
+  instance_type = "t2.micro"
+  count         = 4
+}
+
+# TODO: provision 2 m4.large EC2 instances named Udacity M4
+
+
+resource "aws_instance" "Udacity-M4" {
+  ami           = "ami-0be2609ba883822ec" # Amazon Linux 2 AMI (HVM), SSD in us east 1
+  instance_type = "m4.large"
+  count         = 2
+}
